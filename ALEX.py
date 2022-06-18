2d386782f015991c3f1410c89bbbcdc04400da4b
Mr-Aleex <107762670+Mr-Aleex@users.noreply.github.com>
2022-06-18 20:31:27 +0500

    Update ALEX.py

--- ALEX.py
+++ ALEX.py
@@ -10,6 +10,6 @@ if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls")
     print(" \033[1;31m   Connect Vpn if Run Error!\033[1;37m")
     from ALEX import Subscription
-    Subscription()()
+    Subscription()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
