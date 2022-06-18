ebeda7b2813b0466bc31b9ea69c0550193eb3def
Mr-Aleex <107762670+Mr-Aleex@users.noreply.github.com>
2022-06-18 20:31:17 +0500

    Update ALEX.py

--- ALEX.py
+++ ALEX.py
@@ -9,8 +9,7 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls")
     print(" \033[1;31m   Connect Vpn if Run Error!\033[1;37m")
-   # os.system('xdg-open https://facebook.com/groups/1017905562448002/');time.sleep(1)
-    from AKING import MrAking
-    MrAking()
+    from ALEX import Subscription
+    Subscription()()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
